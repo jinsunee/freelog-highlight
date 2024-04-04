e7b0043059f461e468e222d21ebdd5c2535bf990
jinsunee <jsunee.dev@gmail.com>
2024-04-04 10:00:14 +0900

    feat: When text is selected, display a highlighter button on the right side.

--- src/pages/content/App.tsx
+++ src/pages/content/App.tsx
@@ -1,13 +1,34 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import HighlighterSvg from "../../assets/svgs/HighlighterSvg";
 import { RangeParams } from "./types";
 import { highlightRange } from "./utils";
 
 export default function App() {
   const [selectedItem, setSelectedItem] = useState<RangeParams>();
+  const [buttonPosition, setButtonPosition] = useState<{
+    left: number;
+    top: number;
+  } | null>(null);
 
-  const handleTextSelection = () => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  function handleRemoveButton() {
+    setButtonPosition(null);
+  }
+
+  const handleTextSelection = useCallback((e: MouseEvent) => {
     const selection = window.getSelection();
-    if (!selection || !selection.rangeCount || selection.isCollapsed) return;
+    if (!selection || !selection.rangeCount || selection.isCollapsed) {
+      handleRemoveButton();
+      return;
+    }
+
+    const buttonPosition = {
+      left: e.clientX - 4,
+      top: e.clientY - 40,
+    };
+
+    setButtonPosition(buttonPosition);
 
     const range = selection.getRangeAt(0);
     const startNode = range.startContainer;
@@ -19,20 +40,47 @@ export default function App() {
       start: { node: startNode, offset: startOffset },
       end: { node: endNode, offset: endOffset },
     });
-  };
-
-  useEffect(() => {
-    document.addEventListener("mouseup", handleTextSelection);
-    return () => {
-      document.removeEventListener("mouseup", handleTextSelection);
-    };
   }, []);
 
-  useEffect(() => {
+  const handleHighlight = useCallback(() => {
     if (selectedItem) {
       highlightRange(selectedItem);
+      const selection = window.getSelection();
+      selection?.removeAllRanges();
+      handleRemoveButton();
     }
   }, [selectedItem]);
 
-  return <></>;
+  useEffect(() => {
+    document.addEventListener("mouseup", handleTextSelection);
+    document.addEventListener("scroll", handleRemoveButton);
+
+    return () => {
+      document.removeEventListener("mouseup", handleTextSelection);
+      document.removeEventListener("scroll", handleRemoveButton);
+    };
+  }, [handleTextSelection]);
+
+  return (
+    <>
+      {buttonPosition != null && (
+        <button
+          ref={buttonRef}
+          style={{
+            position: "fixed",
+            left: buttonPosition.left,
+            top: buttonPosition.top,
+            zIndex: 10000,
+            backgroundColor: "#101010",
+            border: "1px solid #bdbaba",
+            borderRadius: "5px",
+            padding: "5px",
+          }}
+          onClick={handleHighlight}
+        >
+          <HighlighterSvg />
+        </button>
+      )}
+    </>
+  );
 }
